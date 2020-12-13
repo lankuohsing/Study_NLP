e51355ebe977d8f752e0d45b657cf7173515814f
lankuohsing <lankuohsing@163.com>
2020-12-13 23:38:12 +0800

    add tf-idf

--- boyu_learning_platform/language_model/language_model.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Feb 19 21:44:03 2020
-
-@author: lankuohsing
-"""
-
-# In[]
---
+++ tf_idf/TfidfVectorizer_usages.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 13 00:21:22 2020
+
+@author: lankuohsing
+"""
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+corpus = [
+    'This is the first document.',
+    'This document is the second document.',
+    'And this is the third one.',
+    'Is this the first document?',
+]
+vectorizer = TfidfVectorizer()
+X = vectorizer.fit_transform(corpus)
+print(vectorizer.get_feature_names())
+
+print(X.shape)
